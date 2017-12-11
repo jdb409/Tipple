02a6b17e24db0eddf2cac91be7a0b991c44217bc
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-12-10 19:42:37 -0500

    cleaned

--- server/models/Cocktail.js
+++ server/models/Cocktail.js
@@ -17,44 +17,5 @@ const Cocktail = db.define('cocktail', {
     }
 })
 
-//not working
-// Cocktail.findMatch = function (barCart) {
-//     return Cocktail.findAll({
-//     }).then(cocktails => {
-//         let diff = [], matches = {};
-//         matches.exact = [];
-//         matches.oneOff = [];
-//         matches.twoOff = [];
-//         cocktails.forEach((cocktail, index) => {
-//             const ingArr = [];
-//             return cocktail.getIngredients({ attributes: ['name'] })
-//                 .then(ing => {
-//                     ing.forEach(ingredient => {
-//                         ingArr.push(ingredient.name)
-//                     })
-//                     diff = _.difference(ingArr, barCart);
-//                     if (!diff.length) {
-//                         matches.exact.push(cocktail);
-
-//                     } else if (diff.length === 1) {
-//                         matches.oneOff.push(cocktail);
-//                     } else if (diff.length === 2) {
-//                         matches.twoOff.push(cocktail);
-//                     }
-
-//                     return matches;
-//                 }).then(matches => {
-//                     if (index === cocktails.length - 1) {
-//                         return matches
-//                     } else {
-//                         return;
-//                     }
-
-//                 })
-//         })
-
-//     })
-// }
-
 module.exports = Cocktail;
 
